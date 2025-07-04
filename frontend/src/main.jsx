8554f53ee445923a8e94f2ea646da3d20c0cc945
Cole Huetten <cole.huetten@du.edu>
2025-07-04 09:19:39 -0500

    Initial setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';    // ← make sure this line is present!
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App/></React.StrictMode>
+);
